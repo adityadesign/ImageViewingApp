966339bab0fe7d1ff26f7dde819e80d8059cf246
Aditya S Acharya <aditya.s.acharya3@gmail.com>
2023-10-19 18:49:12 +0530

    initial commit

---
+++ app/HomePage.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native';
+
+const HomePage = () => {
+  return (
+    <Text>HomePage</Text>
+  )
+}
+
+export default HomePage
